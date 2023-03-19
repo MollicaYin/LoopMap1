c86de6c2e3fc7c902ab3c057810737cd2269802f
MollicaYin <123215908+MollicaYin@users.noreply.github.com>
2023-03-19 16:15:30 -0700

    Update LoopMap1.java

--- LoopMap1.java
+++ LoopMap1.java
@@ -1,1 +1,13 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class NumberStats {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        System.out.println("Enter 5 numbers:");
+        for (int i = 0; i < 5; i++) {
+            int num = scanner.nextInt();
+            numbers.add(num);
+        }
