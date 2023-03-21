922a9e879b19f31780294434a12d76ea049c976b
Mollica Yin <mollicay@gmail.com>
2023-03-20 18:28:15 -0700

    commit update

--- CarType.java
+++ CarType.java
@@ -4,4 +4,25 @@ import java.util.Scanner; // import Scanner
 
 public class CarType {
     
+public static void main(String[] args) {
+    HashMap<String, String> vehicles = new HashMap<>();
+    vehicles.put("Civic", "Honda");
+    vehicles.put("Corolla", "Toyota");
+    vehicles.put("Camry", "Toyota");
+    vehicles.put("Accord", "Honda");
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("What car model are you looking for? ");
+    String model = scanner.nextLine();
+
+    if (vehicles.containsKey(model)) {
+        String make = vehicles.get(model);
+        System.out.println("Oh, you're looking for a " + model + "? Our " + make + " selection is right over here...");
+    } else {
+        System.out.println("Sorry, we don't have that model in stock.");
+    }
+
+    scanner.close();
+}
 }
