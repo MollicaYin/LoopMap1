26a4826d42f625970d4a41002b19fd20ff55a222
Mollica Yin <mollicay@gmail.com>
2023-03-20 17:22:02 -0700

    loops and arrays first practice

---
+++ NumberStats.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class NumberStats {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        System.out.println("Enter 5 numbers:");
+        for (int i = 0; i < 5; i++) {
+            int num = scanner.nextInt();
+            numbers.add(num);
+        }
+        int sum = 0;
+        int product = 1;
+        int largest = numbers.get(0);
+        int smallest = numbers.get(0);
+
+        for (int num : numbers) {
+            sum += num;
+            product *= num;
+            if (num > largest) {
+                largest = num;
+            }
+            if (num < smallest) {
+                smallest = num;
+            }
+        }
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Product: " + product);
+        System.out.println("Largest: " + largest);
+        System.out.println("Smallest: " + smallest);
+
+        scanner.close();
+    }
+}
